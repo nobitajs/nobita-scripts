541e12258b069e2bd22734b86b102a9ea2d2f8a4
JJ <tangjun@zuzuche.com>
2019-08-06 14:08:02 +0800

    v0.1.1

--- bin/nobita-scripts.js
+++ bin/nobita-scripts.js
@@ -1,29 +1,40 @@
 #!/usr/bin/env node
 const program = require('commander');
 const cluster = require('cluster');
+const { spawn } = require('child_process');
 const Master = require('../lib/master.js');
 const Worker = require('../lib/worker.js');
 const Local = require('../lib/local.js');
 const pid = require('../lib/pid.js');
 
-
 program
-	.version('0.1.0', '-v, --version')
+	.version('0.1.1', '-v, --version')
 	.option('-i [value]', '进程数')
 	.option('-e [value]', '运行环境')
 	.option('-n [value]', '应用名称')
-	.option('--silent [boolean]', '是否输出到控制台')
 	.option('--ignored [value]', '忽略文件')
+	.option('--detached [boolean]', '是否后台运行')
 
 program
 	.command('prod [dir]')
+	.action((dir, { parent }) => {
+		const rawArgs = parent.rawArgs.slice(3);
+		spawn('nobita-scripts', ['init', ...rawArgs], {
+			cwd: process.cwd(),
+			detached: !!parent.detached,
+			stdio: !!parent.detached ? 'ignore' : 'inherit'
+		});
+		!!parent.detached && process.exit();
+	});
+
+program
+	.command('init [dir]')
 	.action((dir, { parent }) => {
 		const { N: title = 'app' } = parent;
 		if (cluster.isMaster) {
 			pid.set({ title, pid: process.pid })
 			new Master({ cluster, parent });
 		} else {
-			
 			new Worker({ cluster, parent, dir });
 		}
 	});

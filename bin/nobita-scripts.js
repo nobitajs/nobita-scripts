c32519279e4098bcbb14b6e6f5245c614e3a9ac9
JJ <tangjun@zuzuche.com>
2019-09-09 18:18:45 +0800

    v0.3.4

--- bin/nobita-scripts.js
+++ bin/nobita-scripts.js
@@ -1,28 +1,32 @@
 #!/usr/bin/env node
-const program = require('commander');
+const commander = require('commander');
 
-program
-	.version('0.2.2', '-v, --version')
+commander
+	.version('0.3.3', '-v, --version')
+
+commander
+	.command('start [dir]')
 	.option('-i --instances [value]', '进程数')
 	.option('-e --env [value]', '运行环境')
 	.option('-n --name [value]', '应用名称')
-	.option('--ignored [value]', '忽略文件')
 	.option('-d --detached [boolean]', '是否后台运行')
-
-program
-	.command('start [dir]')
+	.description('以本地生产模式启动应用')
 	.action(require('../lib/cmd/start'));
 
-program
+commander
 	.command('stop [name]')
+	.description('停止应用')
 	.action(require('../lib/cmd/stop'));
 
-program
+commander
 	.command('local [dir]')
+	.option('--ignored [value]', '忽略文件')
+	.description('以本地开发模式启动应用')
 	.action(require('../lib/cmd/local'))
 
-program
+commander
 	.command('list [name]')
+	.description('查看应用列表')
 	.action(require('../lib/cmd/list'))
 
-program.parse(process.argv);
+commander.parse(process.argv);
